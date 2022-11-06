53d51e2e0c4be37ed93bdc1fd55ec9e05ad6323e
Guillermo Castro <guillermo.castro.19@um.edu.mt>
2022-11-06 17:39:52 +0100

    Setup initial webdriver test

--- src/test/java/test/cps3230/web/WebTests.java
+++ src/test/java/test/cps3230/web/WebTests.java
@@ -1,17 +1,28 @@
 package test.cps3230.web;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebTests {
 
+    WebDriver driver;
+
     @BeforeEach
     public void setup() {
-        
+        System.setProperty("webdriver.chrome.driver", "C:/Users/memos/webtesting/chromedriver/chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearddown() {
+        driver.quit();
     }
 
     @Test
     public void testWebsiteSearch() {
-
+        driver.get("https://www.merchbar.com/");
     }
 }
