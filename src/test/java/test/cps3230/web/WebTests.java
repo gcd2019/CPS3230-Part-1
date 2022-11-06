c736b6eec3ca7d4a545df9040817a71823db6c23
Guillermo Castro <guillermo.castro.19@um.edu.mt>
2022-11-06 17:23:35 +0100

    Setup project

---
+++ src/test/java/test/cps3230/web/WebTests.java
@@ -0,0 +1,17 @@
+package test.cps3230.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WebTests {
+
+    @BeforeEach
+    public void setup() {
+        
+    }
+
+    @Test
+    public void testWebsiteSearch() {
+
+    }
+}
