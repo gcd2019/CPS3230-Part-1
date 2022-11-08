1eae488507185ef5a14158fd176795ed7f0b6675
Guillermo Castro <guillermo.castro.19@um.edu.mt>
2022-11-08 19:12:19 +0100

    Create new method to search for products on website

--- src/test/java/test/cps3230/website/pageObjects/WebsitePageObject.java
+++ src/test/java/test/cps3230/website/pageObjects/WebsitePageObject.java
@@ -1,6 +1,8 @@
 package test.cps3230.website.pageObjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class WebsitePageObject {
 
@@ -8,5 +10,12 @@ public class WebsitePageObject {
 
     public WebsitePageObject(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void pressSearch(String product) {
+        WebElement searchField = driver.findElement(By.name("q"));
+        searchField.sendKeys(product);
+        WebElement searchButton = driver.findElement(By.className("actions"));
+        searchButton.submit();
     }
 }
--- src/test/java/test/cps3230/website/tests/WebTests.java
+++ src/test/java/test/cps3230/website/tests/WebTests.java
@@ -3,17 +3,23 @@ package test.cps3230.website.tests;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import test.cps3230.website.pageObjects.WebsitePageObject;
 
 public class WebTests {
 
     WebDriver driver;
+    WebsitePageObject websitePageObject;
 
     @BeforeEach
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "C:/Users/memos/webtesting/chromedriver/chromedriver.exe");
         driver = new ChromeDriver();
+
+        websitePageObject = new WebsitePageObject(driver);
     }
 
     @AfterEach
@@ -24,6 +30,7 @@ public class WebTests {
     @Test
     public void testWebsiteSearch() throws Exception {
         driver.get("https://www.scanmalta.com/shop/");
-        Thread.sleep(5000);
+        websitePageObject.pressSearch("laptop");
+        Thread.sleep(10000);
     }
 }
